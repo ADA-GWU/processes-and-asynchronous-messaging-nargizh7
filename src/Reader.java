a3b0207f4d3d55b2075da76841b4b973df916443
nargizh7 <nargizheybatova07@gmail.com>
2023-10-29 14:10:47 +0400

    working on the code

--- src/Reader.java
+++ src/Reader.java
@@ -1,73 +1,72 @@
-import java.io.*; 
-import java.sql.*; 
-import java.util.*; 
+import java.io.*;
+import java.sql.*;
+import java.util.*;
 import java.util.concurrent.*;
 
 public class Reader {
-	private static List<String> dbServers;
-	private static ExecutorService executor;
-	private static final String READER_NAME = "NargizH";
+    private static List<String> dbServers;
+    private static ExecutorService executor;
+    private static final String READER_NAME = "NargizH";
 
-	public static void main(String[] args) {
-	    dbServers = new ArrayList<>();
-	    try (BufferedReader br = new BufferedReader(new FileReader("db_servers.txt"))) {
-	        String line;
-	        while ((line = br.readLine()) != null) {
-	            dbServers.add(line.trim());
-	        }
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	        return;
-	    }
+    public static void main(String[] args) {
+        dbServers = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("db_servers.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                dbServers.add(line.trim());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        executor = Executors.newFixedThreadPool(dbServers.size());
 
-	    executor = Executors.newFixedThreadPool(dbServers.size());
+        while (true) {
+            for (String dbServer : dbServers) {
+                executor.execute(new CheckTask(dbServer));
+            }
 
-	    while (true) {
-	        for (String dbServer : dbServers) {
-	            executor.execute(new CheckTask(dbServer));
-	        }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
 
-	        try {
-	            Thread.sleep(1000);
-	        } catch (InterruptedException e) {
-	            e.printStackTrace();
-	            break;
-	        }
-	    }
+        executor.shutdown();
+    }
 
-	    executor.shutdown();
-	}
+    static class CheckTask implements Runnable {
+        private String dbServer;
 
-	static class CheckTask implements Runnable {
-	    private String dbServer;
-	    public CheckTask(String dbServer) {
-	        this.dbServer = dbServer;
-	    }
+        public CheckTask(String dbServer) {
+            this.dbServer = dbServer;
+        }
 
-	    @Override
-	    public void run() {
-	        try (Connection conn = DriverManager.getConnection("jdbc:postgresql://" + dbServer + ":5432/postgres", "dist_user", "dist_pass_123")) {
-	
-	            try (PreparedStatement stmt = conn.prepareStatement("SELECT RECORD_ID, SENDER_NAME, MESSAGE, SENT_TIME FROM ASYNC_MESSAGES WHERE RECEIVED_TIME IS NULL AND SENDER_NAME != ? LIMIT 1")) {
-	                stmt.setString(1, READER_NAME);
-	                ResultSet rs = stmt.executeQuery();
+        @Override
+        public void run() {
+            try (Connection conn = DriverManager.getConnection("jdbc:postgresql://" + dbServer + ":5432/postgres", "dist_user", "dist_pass_123")) {
+                try (PreparedStatement stmt = conn.prepareStatement("SELECT RECORD_ID, SENDER_NAME, MESSAGE, SENT_TIME FROM ASYNC_MESSAGES WHERE RECEIVED_TIME IS NULL AND SENDER_NAME != ? LIMIT 1 FOR UPDATE")) {
+                    stmt.setString(1, READER_NAME);
+                    ResultSet rs = stmt.executeQuery();
 
-	                if (rs.next()) {
-	                    int recordId = rs.getInt("RECORD_ID");
-	                    String senderName = rs.getString("SENDER_NAME");
-	                    String message = rs.getString("MESSAGE");
-	                    Timestamp sentTime = rs.getTimestamp("SENT_TIME");
-	                    System.out.println("Sender " + senderName + " sent '" + message + "' at " + sentTime + " from " + dbServer);
-	                    try (PreparedStatement updateStmt = conn.prepareStatement("UPDATE ASYNC_MESSAGES SET RECEIVED_TIME = CURRENT_TIMESTAMP WHERE RECORD_ID = ?")) {
-	                        updateStmt.setInt(1, recordId);
-	                        updateStmt.executeUpdate();
-	                    }
-	                }
-	            }
-	        } catch (SQLException e) {
-	            e.printStackTrace();
-	        }
-	    }
-	}
-
+                    if (rs.next()) {
+                        int recordId = rs.getInt("RECORD_ID");
+                        String senderName = rs.getString("SENDER_NAME");
+                        String message = rs.getString("MESSAGE");
+                        Timestamp sentTime = rs.getTimestamp("SENT_TIME");
+                        System.out.println("Sender " + senderName + " sent '" + message + "' at " + sentTime + " from " + dbServer);
+                        try (PreparedStatement updateStmt = conn.prepareStatement("UPDATE ASYNC_MESSAGES SET RECEIVED_TIME = CURRENT_TIMESTAMP WHERE RECORD_ID = ?")) {
+                            updateStmt.setInt(1, recordId);
+                            updateStmt.executeUpdate();
+                        }
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
--- src/Sender.java
+++ src/Sender.java
@@ -1,64 +1,62 @@
-import java.io.*; 
-import java.sql.*; 
-import java.util.*; 
+import java.io.*;
+import java.sql.*;
+import java.util.*;
 import java.util.concurrent.*;
 
 public class Sender {
-	private static List<String> dbServers;
-	private static ExecutorService executor;
-	private static Scanner scanner;
-	private static final String SENDER_NAME = "NargizH";
+    private static List<String> dbServers;
+    private static ExecutorService executor;
+    private static Scanner scanner;
+    private static final String SENDER_NAME = "NargizH";
 
-	public static void main(String[] args) {
-	    dbServers = new ArrayList<>();
-	    try (BufferedReader br = new BufferedReader(new FileReader("db_servers.txt"))) {
-	        String line;
-	        while ((line = br.readLine()) != null) {
-	            dbServers.add(line.trim());
-	        }
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	        return;
-	    }
-	    executor = Executors.newFixedThreadPool(dbServers.size());
-	    scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        dbServers = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("db_servers.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                dbServers.add(line.trim());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        executor = Executors.newFixedThreadPool(dbServers.size());
+        scanner = new Scanner(System.in);
 
-	    while (true) {
-	        System.out.print("Enter a message or exit: ");
-	        String message = scanner.nextLine();
-	        if (message.equalsIgnoreCase("exit")) {
-	            break;
-	        }
-	        int index = ThreadLocalRandom.current().nextInt(dbServers.size());
-	        executor.execute(new InsertTask(dbServers.get(index), message));
-	    }
-	    executor.shutdown();
-	    scanner.close();
-	}
+        while (true) {
+            System.out.print("Enter a message or exit: ");
+            String message = scanner.nextLine();
+            if (message.equalsIgnoreCase("exit")) {
+                break;
+            }
+            int index = new Random().nextInt(dbServers.size());
+            executor.execute(new InsertTask(dbServers.get(index), message));
+        }
+        executor.shutdown();
+        scanner.close();
+    }
 
-	static class InsertTask implements Runnable {
-	    private String dbServer;
+    static class InsertTask implements Runnable {
+        private String dbServer;
+        private String message;
 
-	    private String message;
+        public InsertTask(String dbServer, String message) {
+            this.dbServer = dbServer;
+            this.message = message;
+        }
 
-	    public InsertTask(String dbServer, String message) {
-	        this.dbServer = dbServer;
-	        this.message = message;
-	    }
-
-	    @Override
-	    public void run() {
-	        try (Connection conn = DriverManager.getConnection("jdbc:postgresql://" + dbServer + ":5432/postgres", "dist_user", "dist_pass_123")) {
-	            try (PreparedStatement stmt = conn.prepareStatement("INSERT INTO ASYNC_MESSAGES (SENDER_NAME, MESSAGE, SENT_TIME) VALUES (?, ?, CURRENT_TIMESTAMP)")) {
-	                stmt.setString(1, SENDER_NAME);
-	                stmt.setString(2, message);
-	                stmt.executeUpdate();
-	                System.out.println("Inserted message '" + message + "' into " + dbServer);
-	            }
-	        } catch (SQLException e) {
-	            e.printStackTrace();
-	        }
-	    }
-	}
-
+        @Override
+        public void run() {
+            try (Connection conn = DriverManager.getConnection("jdbc:postgresql://" + dbServer + ":5432/postgres", "dist_user", "dist_pass_123")) {
+                try (PreparedStatement stmt = conn.prepareStatement("INSERT INTO ASYNC_MESSAGES (SENDER_NAME, MESSAGE, SENT_TIME) VALUES (?, ?, CURRENT_TIMESTAMP)")) {
+                    stmt.setString(1, SENDER_NAME);
+                    stmt.setString(2, message);
+                    stmt.executeUpdate();
+                    System.out.println("Inserted message '" + message + "' into " + dbServer);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
