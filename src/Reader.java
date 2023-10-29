ea59a7e0a1d9f486954c1b25ae9af5657325f706
nargizh7 <nargizheybatova07@gmail.com>
2023-10-29 13:02:47 +0400

    working on the code

--- src/Reader.java
+++ src/Reader.java
@@ -4,17 +4,11 @@ import java.util.*;
 import java.util.concurrent.*;
 
 public class Reader {
-	// A list of database server IPs
 	private static List<String> dbServers;
-
-	// A thread pool to execute tasks
 	private static ExecutorService executor;
-
-	// The reader name
 	private static final String READER_NAME = "NargizH";
 
 	public static void main(String[] args) {
-	    // Initialize the list of database servers from a file
 	    dbServers = new ArrayList<>();
 	    try (BufferedReader br = new BufferedReader(new FileReader("db_servers.txt"))) {
 	        String line;
@@ -26,16 +20,13 @@ public class Reader {
 	        return;
 	    }
 
-	    // Initialize the thread pool with the same size as the number of database servers
 	    executor = Executors.newFixedThreadPool(dbServers.size());
 
-	    // Loop until the user presses Ctrl+C
 	    while (true) {
-	        // Check for available messages in each database server in parallel
 	        for (String dbServer : dbServers) {
 	            executor.execute(new CheckTask(dbServer));
 	        }
-	        // Wait for 1 second before checking again
+
 	        try {
 	            Thread.sleep(1000);
 	        } catch (InterruptedException e) {
@@ -44,29 +35,23 @@ public class Reader {
 	        }
 	    }
 
-	    // Shutdown the thread pool
 	    executor.shutdown();
 	}
 
-	// A task that checks for available messages in a database server
 	static class CheckTask implements Runnable {
-
-	    // The database server IP
 	    private String dbServer;
-
 	    public CheckTask(String dbServer) {
 	        this.dbServer = dbServer;
 	    }
 
 	    @Override
 	    public void run() {
-	        // Create a connection to the database server
 	        try (Connection conn = DriverManager.getConnection("jdbc:postgresql://" + dbServer + ":5432/postgres", "dist_user", "dist_pass_123")) {
-	            // Create a statement to select an available message from the ASYNC_MESSAGES table
+	
 	            try (PreparedStatement stmt = conn.prepareStatement("SELECT RECORD_ID, SENDER_NAME, MESSAGE, SENT_TIME FROM ASYNC_MESSAGES WHERE RECEIVED_TIME IS NULL AND SENDER_NAME != ? LIMIT 1")) {
 	                stmt.setString(1, READER_NAME);
 	                ResultSet rs = stmt.executeQuery();
-	                // If there is an available message, show it on the terminal and update the received time
+
 	                if (rs.next()) {
 	                    int recordId = rs.getInt("RECORD_ID");
 	                    String senderName = rs.getString("SENDER_NAME");
--- src/Sender.java
+++ src/Sender.java
@@ -4,20 +4,12 @@ import java.util.*;
 import java.util.concurrent.*;
 
 public class Sender {
-	// A list of database server IPs
 	private static List<String> dbServers;
-
-	// A thread pool to execute tasks
 	private static ExecutorService executor;
-
-	// A scanner to read user input
 	private static Scanner scanner;
-
-	// The sender name
 	private static final String SENDER_NAME = "NargizH";
 
 	public static void main(String[] args) {
-	    // Initialize the list of database servers from a file
 	    dbServers = new ArrayList<>();
 	    try (BufferedReader br = new BufferedReader(new FileReader("db_servers.txt"))) {
 	        String line;
@@ -28,37 +20,25 @@ public class Sender {
 	        e.printStackTrace();
 	        return;
 	    }
-
-	    // Initialize the thread pool with the same size as the number of database servers
 	    executor = Executors.newFixedThreadPool(dbServers.size());
-
-	    // Initialize the scanner to read user input
 	    scanner = new Scanner(System.in);
 
-	    // Loop until the user enters "exit"
 	    while (true) {
 	        System.out.print("Enter a message or exit: ");
 	        String message = scanner.nextLine();
 	        if (message.equalsIgnoreCase("exit")) {
 	            break;
 	        }
-	        // Choose a random thread to insert the message into a random database server
 	        int index = new Random().nextInt(dbServers.size());
 	        executor.execute(new InsertTask(dbServers.get(index), message));
 	    }
-
-	    // Shutdown the thread pool and close the scanner
 	    executor.shutdown();
 	    scanner.close();
 	}
 
-	// A task that inserts a message into a database server
 	static class InsertTask implements Runnable {
-
-	    // The database server IP
 	    private String dbServer;
 
-	    // The message to insert
 	    private String message;
 
 	    public InsertTask(String dbServer, String message) {
@@ -68,9 +48,7 @@ public class Sender {
 
 	    @Override
 	    public void run() {
-	        // Create a connection to the database server
 	        try (Connection conn = DriverManager.getConnection("jdbc:postgresql://" + dbServer + ":5432/postgres", "dist_user", "dist_pass_123")) {
-	            // Create a statement to insert the message into the ASYNC_MESSAGES table
 	            try (PreparedStatement stmt = conn.prepareStatement("INSERT INTO ASYNC_MESSAGES (SENDER_NAME, MESSAGE, SENT_TIME) VALUES (?, ?, CURRENT_TIMESTAMP)")) {
 	                stmt.setString(1, SENDER_NAME);
 	                stmt.setString(2, message);
